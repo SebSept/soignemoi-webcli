ef70eb6157cf33a161af88e24473b92f3aae2627
Sébastien Monterisi <sebastienmonterisi@yahoo.fr>
2024-05-17 11:09:44 +0200

    menu présentable

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
 import './styles/app.css';
 // inclusion css bootstrap - https://symfony.com/doc/current/frontend/asset_mapper.html#handling-css
 import './vendor/bootstrap/dist/css/bootstrap.min.css';
+import './vendor/bootstrap/bootstrap.index.js';
