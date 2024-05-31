205793638e6b8bd942ace3e12aa8cf31d335cec1
Sébastien Monterisi <sebastienmonterisi@yahoo.fr>
2024-05-31 17:51:26 +0200

    fix assets qui avait sauté!

--- importmap.php
+++ importmap.php
@@ -14,4 +14,14 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    'bootstrap' => [
+        'version' => '5.3.3',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.3',
+        'type' => 'css',
+    ],
 ];
