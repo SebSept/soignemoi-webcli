b453f91c1327379798a7f0e4643f189926a292e4
Sébastien Monterisi <sebastienmonterisi@yahoo.fr>
2024-05-31 11:04:05 +0200

    fix par scripts composer

--- importmap.php
+++ importmap.php
@@ -14,14 +14,4 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
-    'bootstrap' => [
-        'version' => '5.3.3',
-    ],
-    '@popperjs/core' => [
-        'version' => '2.11.8',
-    ],
-    'bootstrap/dist/css/bootstrap.min.css' => [
-        'version' => '5.3.3',
-        'type' => 'css',
-    ],
 ];
