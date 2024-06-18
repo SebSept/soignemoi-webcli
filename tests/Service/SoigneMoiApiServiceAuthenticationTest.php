2501f2d32bced1ffd38c5efdcbdaa1d26d850594
SebSept <sebastienmonterisi@yahoo.fr>
2024-06-18 14:13:35 +0200

    nettoyage des tests

--- tests/Service/SoigneMoiApiServiceAuthenticationTest.php
+++ tests/Service/SoigneMoiApiServiceAuthenticationTest.php
@@ -2,10 +2,7 @@
 
 namespace App\Tests\Service;
 
-use App\Security\User;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-use App\Entity\HospitalStay;
 use App\Service\SoigneMoiApiService;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\HttpClient\MockHttpClient;
@@ -14,7 +11,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class SoigneMoiApiServiceAuthenticationTest extends KernelTestCase
 {
-    
     public function testAuthenticationFailsIfUnAuthorized(): void
     {
         // Arrange
@@ -136,33 +132,6 @@ class SoigneMoiApiServiceAuthenticationTest extends KernelTestCase
         $this->assertSame('ROLE_PATIENT', $response->role);
         $this->assertSame($token, $response->token);
         $this->assertSame($id, $response->id);
-    }
-
-    public function testGetPatientHospitalStays(): void
-    {
-        $httpClient = new MockHttpClient();
-        $mockResponse = new MockResponse(
-            file_get_contents(__DIR__.'/response_hospital_stays.json'),
-            ['http_code' => Response::HTTP_OK]
-        );
-        $httpClient->setResponseFactory(static fn(): MockResponse => $mockResponse);
-
-        $user = new User('nop@nop.com');
-        $user->setToken('123');
-        $user->setId(7);
-
-        $mockedSecurity = $this->createMock(Security::class);
-        $mockedSecurity->method('getUser')->willReturn($user);
-
-        static::getContainer()->set(HttpClientInterface::class, $httpClient);
-        static::getContainer()->set(Security::class, $mockedSecurity);
-
-        /** @var SoigneMoiApiService $api */
-        $api = static::getContainer()->get(SoigneMoiApiService::class);
-
-        $hospitalStays = $api->getPatientHospitalStays();
-
-        $this->assertContainsOnlyInstancesOf(HospitalStay::class, $hospitalStays);
     }
 
     public function testPatientCanLogin(): void
